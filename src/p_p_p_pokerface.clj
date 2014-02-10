f10f3e71837d76d48936e428b3d8f8ced036773f
Thomas B Homburg <thomas@homburg.dk>
2014-02-10 23:08:53 +0100

    Add solution

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,72 @@
 (ns p-p-p-pokerface)
 
-(defn rank [card]
-  nil)
+(defn face->value [face]
+  "Convert card face to value"
+  (get (zipmap [\T \J \Q \K \A] (range 10 15)) face))
 
-(defn suit [card]
-  nil)
+(defn rank [[r]]
+  (if (Character/isDigit r)
+    (Integer/valueOf (str r))
+    (face->value r)))
+
+(defn suit [[_ s]]
+  (str s))
+
+(defn card-n? [hand n]
+  "Verify matching cards at least n"
+  (boolean (some #(<= n %) (vals (frequencies (map rank hand))))))
 
 (defn pair? [hand]
-  nil)
+  (card-n? hand 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (card-n? hand 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (card-n? hand 4))
 
 (defn flush? [hand]
-  nil)
+   (= 1 (count (frequencies (map suit hand)))))
 
 (defn full-house? [hand]
-  nil)
+  (= #{2 3}
+     (set (vals (frequencies (map rank hand))))))
 
 (defn two-pairs? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        freqs (->> ranks frequencies vals)
+        pairs (filter #(= 2 %) freqs)]
+    (= 2 (count pairs))))
 
 (defn straight? [hand]
-  nil)
+  (letfn [(straight-helper? [ranks]
+            (let [n (count ranks)
+                  m (apply min ranks)
+                  real-straight-ranks (range m (+ n m))]
+              (= (sort ranks) real-straight-ranks)))]
+    (let [ranks (map rank hand)]
+      (or
+        (straight-helper? ranks)
+        (straight-helper? (replace {14 1} ranks))))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand)
+       (flush? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [checkers (list [straight-flush? 8]
+                       [four-of-a-kind? 7]
+                       [full-house? 6]
+                       [flush? 5]
+                       [straight? 4]
+                       [three-of-a-kind? 3]
+                       [two-pairs? 2]
+                       [pair? 1]
+                       [high-card? 0])
+        matched-checker (first (filter #((first %) hand) checkers))]
+    (second matched-checker)))
+
+
